f7fe09f0892bede0eba76f13de13a7f85b9ce53a
Mark Stuver <mark@stuverfamily.com>
2014-09-18 21:34:39 -0400

    Control Flow Playground

---
+++ ControlFlow.playground/section-1.swift
@@ -0,0 +1,57 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+let truckSpeed = 45
+let lamboSpeed = 120
+var mySpeed:Int
+
+mySpeed = 69
+
+//If STATEMENT
+        // if myspeed is less then 70 print this
+        if mySpeed < 70 {
+        println("Keep Crusin'")
+        }
+
+//ELSE STATEMENT
+            // else print this
+        else {
+            println("Slow down police ahead")
+        }
+
+//BOOLs
+        //Bools are either true or false. Naming convention is asking a true or false (yes or no)
+        let isTelevisionOn = true
+
+        //if isTelevisionOn is true... print this
+        if isTelevisionOn == true {
+            print("Make sure to turn of when done")
+        }
+        // else print this
+        else {
+            print("Do you want to use this now")
+        }
+
+        /// also can use else if statements
+        
+//NUMERIC TYPE CONVERSION
+    
+//to add intValue to doubleValue
+var intValue = 5
+var doubleValue = 0.5
+//need to convert the intValue to a Double
+var sumOfValues = Double(intValue) + doubleValue
+
+//to add doubleValue to intValue
+var secondDoubleValue = 1.5
+var secondIntValue = 8
+
+// need to convert the doubleValue to a Int
+// NOTE: when converting from Double to Int, the decimal value is dropped.
+var secondSumOfValue = Int(secondDoubleValue) + secondIntValue
+
+
+
+
+
--- README.md
+++ README.md
@@ -5,14 +5,22 @@ Playgrounds
 
 ## Variables
 
-###Type Annotation
-###Operations on Variables - INT & Floats/Doubles
-###Type Alias
-###Numeric Literals
-###Strings & Characters
-###String Interpolation
-### Optionals INTS - converting a string to Int
-###Sring to Doubles
-### Short Hand - For Addition/Subtraction/Multiplication/Divison
+####Type Annotation
+####Operations on Variables - INT & Floats/Doubles
+####Type Alias
+####Numeric Literals
+####Strings & Characters
+####String Interpolation
+#### Optionals INTS - converting a string to Int
+####Sring to Doubles
+#### Short Hand - For Addition/Subtraction/Multiplication/Divison
+
+## Control Flow
+
+####If Statements
+####Else Statements
+####Bools
+####Numeric Type Conversion
+
 
 
