69e65f98de10fb6b26c41a55a6497d2d1e1c02d4
Mark Stuver <mark@stuverfamily.com>
2014-09-28 16:29:57 -0400

    Random Number Generator - Playground

---
+++ RandomNumberGenerator.playground/section-1.swift
@@ -0,0 +1,21 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+
+/// code will radomly assign an int between 0 and the number specified int the code. The example below is (3).
+let randomNumber = Int(arc4random_uniform(UInt32(90)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
