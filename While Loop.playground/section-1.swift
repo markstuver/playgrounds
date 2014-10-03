a4313b246f82bc3a06435a6f553a057dbc406488
Mark Stuver <mark@stuverfamily.com>
2014-10-03 13:33:47 -0400

    While Loop vs Do While Loop Playground

---
+++ While Loop.playground/section-1.swift
@@ -0,0 +1,29 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+// While Loops run a bit of code over and over until it's comparison fails.
+
+var x = 5
+
+// In While Loop: Condition is tested first and then the statements are executed if the condition turns out to be true
+
+while x < 10 {
+    println(x)
+    x++
+}
+
+
+
+var y = 10
+
+
+// Do While Loop: Statements are executed for the first time and then the conditions are tested, if the condition turns out to be true, then the statements are executed again.
+do {
+    println(y)
+    y--
+    
+} while y > 0
+
+
+//
